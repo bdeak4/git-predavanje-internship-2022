9c6b631e4d23d313cdc2066f6efe860455a213a9
Bartol Deak <b@bdeak.net>
2022-11-06 15:42:40 +0100

    ide do 150 sad

--- program.js
+++ program.js
@@ -1,4 +1,4 @@
-for (let i = 1; i <= 100; i++) {
+for (let i = 1; i <= 150; i++) {
         if (i % 5 === 0 && i % 3 === 0) console.log("FizzBuzz")
         if (i % 3 === 0) console.log("Fizz")
         if (i % 5 === 0) console.log("Buzz")
