9fba03c4a45bfe6678d044f2a570377511890e27
Bartol Deak <b@bdeak.net>
2022-11-06 15:18:05 +0100

    add program.js

---
+++ program.js
@@ -0,0 +1,6 @@
+for (var i = 0; i <= 100; i++) {
+        if (i % 5 === 0 && i % 3 === 0) console.log("FizzBuzz")
+        if (i % 3 === 0) console.log("Fizz")
+        if (i % 5 === 0) console.log("Buzz")
+        else console.log(i)
+}
