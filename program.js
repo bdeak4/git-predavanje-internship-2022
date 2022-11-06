641f780fce5b971b608d42863831ad92318d9e01
Bartol Deak <b@bdeak.net>
2022-11-06 15:27:06 +0100

    change var to let

--- program.js
+++ program.js
@@ -1,4 +1,4 @@
-for (var i = 0; i <= 100; i++) {
+for (let i = 0; i <= 100; i++) {
         if (i % 5 === 0 && i % 3 === 0) console.log("FizzBuzz")
         if (i % 3 === 0) console.log("Fizz")
         if (i % 5 === 0) console.log("Buzz")
